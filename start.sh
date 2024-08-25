82da962e5013fc7b3d5e94f6df5a0c9d8c1a23b8
say ken <a.meowalien@gmail.com>
2024-08-26 01:14:18 +0800

    update mongodb.url

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#minikube start
-
-#minikube ssh -- "sudo sed -i '0,/{/s/{/{\"insecure-registries\": [\"docker-registry.docker-registry.svc.cluster.local:5000\"],/' /etc/docker/daemon.json"
+minikube start --insecure-registry="docker-registry.docker-registry.svc.cluster.local:5000"
 
 kubectl kubevpn connect -n default
