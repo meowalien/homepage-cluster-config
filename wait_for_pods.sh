90d2185ece78df357795b4bafe65eacea1d76318
say ken <a.meowalien@gmail.com>
2024-09-21 14:39:34 +0800

    edit wait_for_pods.sh

--- wait_for_pods.sh
+++ wait_for_pods.sh
@@ -2,6 +2,8 @@
 
 echo "Waiting for all pods to be ready..."
 
+prev_total_lines=0
+
 while true; do
     # Get all pods in all namespaces without headers
     pods=$(kubectl get pods --all-namespaces --no-headers)
@@ -10,15 +12,31 @@ while true; do
     not_ready_pods=$(echo "$pods" | awk '{split($3, a, "/"); if (a[1] != a[2]) print $0}')
 
     if [ -z "$not_ready_pods" ]; then
+        # Move the cursor up by the number of previous lines
+        if [ "$prev_total_lines" -gt 0 ]; then
+            printf "\033[%dA" "$prev_total_lines"
+        fi
         echo "All pods are ready."
         break
     else
-        # Clear the console output
+        # Count the number of lines to be printed
+        num_pods=$(echo "$not_ready_pods" | wc -l)
+        total_lines=$((3 + num_pods))
+
+        # Move the cursor up by the number of previous lines
+        if [ "$prev_total_lines" -gt 0 ]; then
+            printf "\033[%dA" "$prev_total_lines"
+        fi
+
+        # Print the updated status
         echo "Waiting for all pods to be ready..."
         echo "========================================"
         echo "The following pods are not ready yet:"
         echo "$not_ready_pods"
+
+        # Update the previous total lines
+        prev_total_lines=$total_lines
+
         sleep 5
-        clear
     fi
 done
